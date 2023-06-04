1940dc77efa5166599671d38cecd303daa7787ec
Pratik <pratiktalware26@gmail.com>
2023-06-04 11:05:42 +0530

    Home transition and scrolling effect added

--- src/Components/AllComponents.jsx
+++ src/Components/AllComponents.jsx
@@ -1,28 +1,81 @@
-import React from 'react'
-import ResidentialProperties from './ResidentialProperties'
-import Navbar from './Navbar'
-import Home from './Home'
-import homeBuyerImg from "../assets/brickf strip1.png"
-import CuratedCollection from './CuratedCollection'
-import InvestmentProperty from './InvestmentProperty'
-import Brands from './Brands'
-import Footer from './Footer'
+import React, { useEffect, useRef } from "react";
+import ResidentialProperties from "./ResidentialProperties";
+import Navbar from "./Navbar";
+import Home from "./Home";
+import homeBuyerImg from "../assets/brickf strip1.png";
+import CuratedCollection from "./CuratedCollection";
+import InvestmentProperty from "./InvestmentProperty";
+import Brands from "./Brands";
+import Footer from "./Footer";
+import "./AllComponents.css";
 
 const AllComponents = () => {
-  return (
-    <div className='nav-home-cont'>
-        <Navbar/>
-        <Home/>
-        <ResidentialProperties/>
-        <div className='home-buyer-img'>
-            <img src={homeBuyerImg} alt="" className='w-100 h-100'/>
-        </div>
-        <CuratedCollection/>
-        <InvestmentProperty/>
-        <Brands/>
-        <Footer/>
-    </div>
-  )
-}
+  useEffect(() => {
+    const animatedComponents = Array.from(
+      document.querySelectorAll(".animated-component")
+    );
+    const observerOptions = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.15, // Adjust the threshold as needed
+    };
 
-export default AllComponents
+    const handleIntersection = (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+        } else {
+          entry.target.classList.remove("visible");
+        }
+      });
+    };
+
+    const observer = new IntersectionObserver(
+      handleIntersection,
+      observerOptions
+    );
+
+    animatedComponents.forEach((component) => {
+      observer.observe(component);
+    });
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
+  return (
+    <div className="nav-home-cont">
+      <Navbar />
+      <Home />
+
+      <div className="animated-component">
+        <ResidentialProperties />
+      </div>
+
+      <div className="animated-component">
+        <div className="home-buyer-img">
+          <img src={homeBuyerImg} alt="" className="w-100 h-100" />
+        </div>
+      </div>
+
+      <div className="animated-component">
+        <CuratedCollection />
+      </div>
+
+      <div className="animated-component">
+        <InvestmentProperty />
+      </div>
+
+      <div className="animated-component">
+        <Brands />
+      </div>
+
+      <div className="animated-component">
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default AllComponents;
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,55 +1,111 @@
-import React from 'react'
-import "./Home.css"
-import searchIcon from "../assets/search.png"
+import React, { useEffect, useState } from "react";
+import "./Home.css";
 
 const Home = () => {
+  const [homeTrans, setHometrans] = useState("");
+  useEffect(() => {
+    const homeSection = document.querySelector(".home-cont");
+    const options = {
+    //   rootMargin: "0px",
+      threshold: 0.3,
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setHometrans("home-cont-trans");
+        } else {
+          setHometrans("");
+        }
+      });
+    }, options);
+
+    observer.observe(homeSection);
+
+    return () => {
+      observer.unobserve(homeSection);
+    };
+  }, []);
   return (
-    <div className='home-cont-parent'>
-        <div className='d-flex flex-column align-items-center home-cont'>
-        <div><h1 className='display-4 main-head'><b>BRICKFOLIO</b> - A LOCAL EXPERT</h1></div>
-        <div className='d-flex flex-column home-btn-opts-cont'>
-            <div className='d-flex justify-content-center home-btn-opts'>
-                <div><button className='home-btns'>Investment</button></div>
-                <div><button className='home-btns'>Commercial</button></div>
-                <div><button className='home-btns'>Resindential</button></div>
-                <div><button className='home-btns'>Managed Offices</button></div>
-            </div>
-            <div className='d-flex home-search-opt-cont'>
-                <div className='select-opt-cont'>
-                    <div>
-                    <select name="" id="" placeholder='Select Area' className='home-select'>
-                        <option>Ajantha Nagar</option>
-                        <option>Akurdi</option>
-                        <option>Ambegaon Budruk</option>
-                    </select>
-                    </div>
-
-                    <div>
-                    <select name="" id="" placeholder='Select Area' className='home-select'>
-                        <option>Ajantha Nagar</option>
-                        <option>Akurdi</option>
-                        <option>Ambegaon Budruk</option>
-                    </select>
-                    </div>
-
-                    <div>
-                    <select name="" id="" placeholder='Select Area' className='home-select'>
-                        <option>Ajantha Nagar</option>
-                        <option>Akurdi</option>
-                        <option>Ambegaon Budruk</option>
-                    </select>
-                    </div>
-
-                    <div><button className='home-search-btn'><i className="fa fa-search"></i></button></div>
-                </div>
-                <div>
-                    <button className='home-btns'>Post Your Property</button>
-                </div>
-            </div>
+    <div className="home-cont-parent">
+      <div
+        className={`d-flex flex-column align-items-center home-cont ${homeTrans}`}
+      >
+        <div>
+          <h1 className="display-4 main-head">
+            <b>BRICKFOLIO</b> - A LOCAL EXPERT
+          </h1>
         </div>
+        <div className="d-flex flex-column home-btn-opts-cont">
+          <div className="d-flex justify-content-center home-btn-opts">
+            <div>
+              <button className="home-btns">Investment</button>
+            </div>
+            <div>
+              <button className="home-btns">Commercial</button>
+            </div>
+            <div>
+              <button className="home-btns">Resindential</button>
+            </div>
+            <div>
+              <button className="home-btns">Managed Offices</button>
+            </div>
+          </div>
+          <div className="d-flex home-search-opt-cont">
+            <div className="select-opt-cont">
+              <div>
+                <select
+                  name=""
+                  id=""
+                  placeholder="Select Area"
+                  className="home-select"
+                >
+                  <option>Ajantha Nagar</option>
+                  <option>Akurdi</option>
+                  <option>Ambegaon Budruk</option>
+                </select>
+              </div>
+
+              <div>
+                <select
+                  name=""
+                  id=""
+                  placeholder="Select Area"
+                  className="home-select"
+                >
+                  <option>Ajantha Nagar</option>
+                  <option>Akurdi</option>
+                  <option>Ambegaon Budruk</option>
+                </select>
+              </div>
+
+              <div>
+                <select
+                  name=""
+                  id=""
+                  placeholder="Select Area"
+                  className="home-select"
+                >
+                  <option>Ajantha Nagar</option>
+                  <option>Akurdi</option>
+                  <option>Ambegaon Budruk</option>
+                </select>
+              </div>
+
+              <div>
+                <button className="home-search-btn">
+                  <i className="fa fa-search"></i>
+                </button>
+              </div>
+            </div>
+            <div>
+              <button className="home-btns">Post Your Property</button>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
